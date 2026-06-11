185a8f887664e3da645f136e1ba4c61fe308a561
Team Pacific Corporation MIS Department <silos.miguel@outlook.com>
2026-06-11 08:07:31 +0800

    base  css

--- adminprofile/static/adminprofile/src/js/script.js
+++ adminprofile/static/adminprofile/src/js/script.js
@@ -1,54 +1,8 @@
+// get api token
 document.getElementById('repo-btn').addEventListener('click', function () {
     window.location.href = "?sync=github";
 });
-const themeBtn = document.getElementById('themeBtn');
-const body = document.body;
 
-// Toggle Theme
-themeBtn.addEventListener('click', () => {
-    const currentTheme = body.getAttribute('data-theme');
-
-    if (currentTheme === 'light') {
-        body.removeAttribute('data-theme');
-        localStorage.setItem('portfolio-theme', 'dark');
-    } else {
-        body.setAttribute('data-theme', 'light');
-        localStorage.setItem('portfolio-theme', 'light');
-    }
-});
-
-if (localStorage.getItem('portfolio-theme') === 'light') {
-    body.setAttribute('data-theme', 'light');
-}
-
-const tabs = document.querySelectorAll(".tab");
-const contents = document.querySelectorAll(".tab-content");
-
-tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-
-        // remove active from all tabs
-        tabs.forEach(t => t.classList.remove("active"));
-
-        // hide all content
-        contents.forEach(c => c.classList.remove("active"));
-
-        // activate clicked tab
-        tab.classList.add("active");
-
-        // show matching content
-        const target = tab.dataset.tab;
-        document.getElementById(target).classList.add("active");
-    });
-});
-
-const burgerBtn = document.getElementById("burgerBtn");
-const mobileNav = document.querySelector(".modern-nav");
-
-burgerBtn.addEventListener("click", () => {
-    mobileNav.classList.toggle("active");
-
-});
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -63,3 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+
+
