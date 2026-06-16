8bf4295a5f8b82944516005d269b1198c188a4d5
Team Pacific Corporation MIS Department <silos.miguel@outlook.com>
2026-06-16 17:52:47 +0800

    update

--- portal/settings.py
+++ portal/settings.py
@@ -81,12 +81,15 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { 'default': 
+                { 'ENGINE': 'django.db.backends.postgresql',
+                    'NAME': 'Django_DB',
+                    'USER': 'django',
+                    'PASSWORD': 'django',
+                    'HOST': 'localhost',
+                    'PORT': '5432',
+                }
+            }
 
 
 # Password validation
