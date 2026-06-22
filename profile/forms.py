afcf6f8c3bdc24c37fd26acf15375d1f4c608253
Miguel <silos.miguel@outlook.com>
2026-06-23 00:55:03 +0800

    access: user edit profile
    access to edit user profile

--- profile/forms.py
+++ profile/forms.py
@@ -1,13 +1,43 @@
 from django import forms
-from .models import Profile
+from .models import Profile, SocialMedia
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['description']
+        fields = ['profile','description']
         widgets = {
             'description': forms.Textarea(attrs={
                 'rows': 5,
+                'class': 'text-area',
+                'placeholder': 'Fullstack Python developer...'
+            }),
+
+             'profile': forms.FileInput(attrs={
+                'class': 'profile-input'
+            })
+        }
+
+
+
+class SocialMediaForm(forms.ModelForm):
+
+    class Meta:
+        model = SocialMedia
+        fields = ['name', 'url', 'logo']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Platform name'
+            }),
+
+            'url': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'https://github.com/username'
+            }),
+
+            'logo': forms.FileInput(attrs={
                 'class': 'form-control'
             })
         }
---
+++ profile/services/github_api.py
@@ -0,0 +1,69 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+from django.contrib.auth.models import User 
+from profile.models import Profile
+
+load_dotenv()
+
+# superuser = User.objects.filter(is_superuser=True).first()
+# profile, created = Profile.objects.get_or_create(user=superuser)
+
+# TOKEN = f'{profile.github_token}'
+
+# if not TOKEN:
+#     raise ValueError("GITHUB_TOKEN is not set in .env file or Github Token is expired!")
+
+# headers = {
+#     "Authorization": f"Bearer {TOKEN}",
+#     "Accept": "application/vnd.github+json"
+# }
+
+def get_headers(user):
+
+    profile, created = Profile.objects.get_or_create(user=user)
+
+    token = profile.github_token
+
+    if not token:
+        raise ValueError("GitHub token is missing.")
+
+    return {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json",
+    }
+
+
+def fetch_personal_repositories(user):
+
+    headers = get_headers(user)
+
+    url = "https://api.github.com/user/repos"
+
+    params = {
+        "visibility": "all",
+        "sort": "updated",
+        "per_page": 100
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        timeout=10
+    )
+
+    return response.json()
+
+GITHUB_API = "https://api.github.com"
+
+def fetch_commits(user, owner, repo):
+    headers = get_headers(user)
+    url = f"{GITHUB_API}/repos/{owner}/{repo}/commits?per_page=100"
+    r = requests.get(url, headers=headers, timeout=30)
+
+    if r.status_code != 200:
+        return []
+
+    return r.json()
